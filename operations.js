1600f6fbda7f419533f45d4a22824e60c6ff023c
DanielChisica <daniel.chisica@hotmail.com>
2018-03-05 22:09:51 -0500

    Verify errors

--- operations.js
+++ operations.js
@@ -1,6 +1,94 @@
-var screen= document.getElementById("screen");
-var numclick = function(){
-}
-var btnNumeros = document.getElementsByClassName("btn-num");
+var screen = document.getElementById("screen");
+var numclick = function () {
+    if(screen.getAttribute("value")==0){
+        screen.setAttribute("value",this.textContent);
+    }else{
+        screen.setAttribute("value",screen.getAttribute("value") + this.textContent);
+    }
+};
+var btnNumbers = document.getElementsByClassName("btn-num");
 
-for(var i=0)
+
+for (var i = 0; i < btnNumbers.length; i++) {
+    btnNumbers[i].onclick = numclick;
+}
+
+var memory = 0;
+var lastOperator = "";
+
+/**
+var add = function () {
+    memory += Number(screen.getAttribute("value"));
+}
+
+var subtract = function () {
+    memory -= Number(screen.getAttribute("value"));
+}
+
+var multiply = function () {
+    memory *= Number(screen.getAttribute("value"));
+}
+
+var divide = function () {
+    memory /= Number(screen.getAttribute("value"));
+}
+*/
+
+
+
+document.getElementById("op:+").onclick = function (){
+
+    memory += Number(screen.getAttribute("value"));
+    screen.setAttribute("value", 0);
+    lastOperator = "+";
+}
+
+document.getElementById("op:-").onclick = function () {
+    memory -= Number(screen.getAttribute("value"));
+    screen.setAttribute("value", 0);
+    lastOperator = "-";
+}
+
+document.getElementById("op:*").onclick = function () {
+    memory *= Number(screen.getAttribute("value"));
+    screen.setAttribute("value", 0);
+    lastOperator = "*";
+}
+
+document.getElementById("op:/").onclick = function () {
+    memory /= Number(screen.getAttribute("value"));
+    screen.setAttribute("value", 0);
+    lastOperator = "/";
+}
+
+document.getElementById("equal").onclick = function () {
+    switch (lastOperator) {
+        case "+":
+            memory += Number(screen.getAttribute("value"))-cache;
+            lastOperator = "";
+            break;
+
+        case "-":
+            memory -= Number(screen.getAttribute("value"));
+            lastOperator = "";
+            break;
+
+        case "*":
+            memory *= Number(screen.getAttribute("value"));
+            lastOperator = "";
+            break;
+
+        case "/":
+            memory /= Number(screen.getAttribute("value"));
+            lastOperator = "";
+            break;
+
+    }
+
+    screen.setAttribute("value",memory);
+};
+
+document.getElementById("clear").onclick = function () {
+    memory = 0;
+    screen.setAttribute("value",0);
+};

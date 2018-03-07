d3fabf5a1b21fbbe517d6213f6359fcf6df41e5d
DanielChisica <daniel.chisica@hotmail.com>
2018-03-06 23:41:30 -0500

    Implement history and design

--- operations.js
+++ operations.js
@@ -1,13 +1,14 @@
 var screen = document.getElementById("screen");
+var history = document.getElementById("history");
 var numclick = function () {
-    if(screen.getAttribute("value")==0){
-        screen.setAttribute("value",this.textContent);
-    }else{
-        screen.setAttribute("value",screen.getAttribute("value") + this.textContent);
+    if (screen.getAttribute("value") == 0) {
+        screen.setAttribute("value", this.textContent);
+    } else {
+        screen.setAttribute("value", screen.getAttribute("value") + this.textContent);
     }
 };
 var btnNumbers = document.getElementsByClassName("btn-num");
-
+var record;
 
 for (var i = 0; i < btnNumbers.length; i++) {
     btnNumbers[i].onclick = numclick;
@@ -36,35 +37,59 @@ var divide = function () {
 
 
 
-document.getElementById("op:+").onclick = function (){
-
-    memory += Number(screen.getAttribute("value"));
+document.getElementById("op:+").onclick = function () {
+    if (memory !== Number(screen.getAttribute("value"))) {
+        memory += Number(screen.getAttribute("value"));
+    }
     screen.setAttribute("value", 0);
     lastOperator = "+";
+    record += memory + " + ";
+    history.setAttribute("value", "funciona");
+
 }
 
 document.getElementById("op:-").onclick = function () {
-    memory -= Number(screen.getAttribute("value"));
+    if (memory !== Number(screen.getAttribute("value"))) {
+        memory -= Number(screen.getAttribute("value"));
+    }
     screen.setAttribute("value", 0);
     lastOperator = "-";
+    record += memory + " - ";
+    history.setAttribute("value", record);
 }
 
 document.getElementById("op:*").onclick = function () {
-    memory *= Number(screen.getAttribute("value"));
+    if (memory !== Number(screen.getAttribute("value"))) {
+        if (memory == 0) {
+            memory = 1;
+        }
+        memory *= Number(screen.getAttribute("value"));
+    }
     screen.setAttribute("value", 0);
     lastOperator = "*";
+    record += memory + " * ";
+
+    history.setAttribute("value", record);
 }
 
 document.getElementById("op:/").onclick = function () {
-    memory /= Number(screen.getAttribute("value"));
+    if (memory !== Number(screen.getAttribute("value"))) {
+        if (memory == 0) {
+            memory = Number(screen.getAttribute("value"));
+        } else {
+            memory /= Number(screen.getAttribute("value"));
+        }
+    }
     screen.setAttribute("value", 0);
     lastOperator = "/";
+    record += memory + " / ";
+    history.setAttribute("value", record);
 }
 
 document.getElementById("equal").onclick = function () {
     switch (lastOperator) {
         case "+":
-            memory += Number(screen.getAttribute("value"))-cache;
+            memory += Number(screen.getAttribute("value"));
             lastOperator = "";
             break;
 
@@ -82,13 +107,14 @@ document.getElementById("equal").onclick = function () {
             memory /= Number(screen.getAttribute("value"));
             lastOperator = "";
             break;
-
     }
 
-    screen.setAttribute("value",memory);
+    screen.setAttribute("value", memory);
+    record += memory + " = " + "\n";
+    history.setAttribute("value", record);
 };
 
 document.getElementById("clear").onclick = function () {
     memory = 0;
-    screen.setAttribute("value",0);
+    screen.setAttribute("value", 0);
 };

e980da26aea5a4089efec70b13729f8305f3f2eb
DanielChisica <daniel.chisica@hotmail.com>
2018-03-08 23:47:56 -0500

    Works fine

--- operations.js
+++ operations.js
@@ -1,5 +1,22 @@
+/**
+ * @file Working with Tags
+ * @author Daniel Jiménez Chísica <djimenez4223@universidadean.edu.co>
+ * @version 0.1
+ */
+
+/**
+ * Gets the text box in the html file that represents the screen
+ */
 var screen = document.getElementById("screen");
-var history = document.getElementById("history");
+
+/**
+ * Gets the text box in the html file that represents the history screen
+ */
+var historial = document.getElementById("history");
+
+/**
+ * This variable defines a function for each number that calculator has
+ */
 var numclick = function () {
     if (screen.getAttribute("value") == 0) {
         screen.setAttribute("value", this.textContent);
@@ -7,9 +24,16 @@ var numclick = function () {
         screen.setAttribute("value", screen.getAttribute("value") + this.textContent);
     }
 };
-var btnNumbers = document.getElementsByClassName("btn-num");
-var record;
 
+/**
+ * Gets all the numbers defined by the class btn-num
+ */
+var btnNumbers = document.getElementsByClassName("btn-num");
+var record = "";
+
+/**
+ * Assigns a function for each number button
+ */
 for (var i = 0; i < btnNumbers.length; i++) {
     btnNumbers[i].onclick = numclick;
 }
@@ -17,104 +41,156 @@ for (var i = 0; i < btnNumbers.length; i++) {
 var memory = 0;
 var lastOperator = "";
 
+
+
 /**
-var add = function () {
-    memory += Number(screen.getAttribute("value"));
-}
-
-var subtract = function () {
-    memory -= Number(screen.getAttribute("value"));
-}
-
-var multiply = function () {
-    memory *= Number(screen.getAttribute("value"));
-}
-
-var divide = function () {
-    memory /= Number(screen.getAttribute("value"));
-}
-*/
-
-
-
+ * Contains the logic of the sum
+ */
 document.getElementById("op:+").onclick = function () {
-    if (memory !== Number(screen.getAttribute("value"))) {
+
+    if (lastOperator == "=") {
+        record += " + ";
+
+    } else {
+        record += Number(screen.getAttribute("value")) + " + ";
+    }
+
+    historial.setAttribute("value", record);
+
+    if (Number(screen.getAttribute("value")) !== 0 && lastOperator !== "=") {
         memory += Number(screen.getAttribute("value"));
     }
     screen.setAttribute("value", 0);
+
+
+
     lastOperator = "+";
-    record += memory + " + ";
-    history.setAttribute("value", "funciona");
+
+
 
 }
 
+/**
+ * Generates the subtraction operation
+ */
 document.getElementById("op:-").onclick = function () {
+
+
+    if (lastOperator == "=") {
+        record += " - ";
+
+    } else {
+        record += Number(screen.getAttribute("value")) + " - ";
+    }
+
+    historial.setAttribute("value", record);
+
+    lastValue = Number(screen.getAttribute("value"));
+
     if (memory !== Number(screen.getAttribute("value"))) {
-        memory -= Number(screen.getAttribute("value"));
+        if (memory != 0) {
+            memory -= Number(screen.getAttribute("value"));
+        } else {
+            memory = Number(screen.getAttribute("value"));
+        }
     }
     screen.setAttribute("value", 0);
     lastOperator = "-";
-    record += memory + " - ";
-    history.setAttribute("value", record);
+
 }
 
+/**
+ * Generates the multiplication operation
+ */
 document.getElementById("op:*").onclick = function () {
-    if (memory !== Number(screen.getAttribute("value"))) {
+    if (lastOperator == "=") {
+        record += " * ";
+
+
+    } else {
+        record += Number(screen.getAttribute("value")) + " * ";
+    }
+
+    historial.setAttribute("value", record);
+
+
+    if (Number(screen.getAttribute("value")) !== 0) {
         if (memory == 0) {
             memory = 1;
         }
-        memory *= Number(screen.getAttribute("value"));
+        if (lastOperator !== "=") {
+            memory *= Number(screen.getAttribute("value"));
+        }
+
     }
     screen.setAttribute("value", 0);
     lastOperator = "*";
-    record += memory + " * ";
-
-    history.setAttribute("value", record);
 }
 
+/**
+ * Sets the division operation
+ */
 document.getElementById("op:/").onclick = function () {
-    if (memory !== Number(screen.getAttribute("value"))) {
+    if (lastOperator == "=") {
+        record += " / ";
+
+    } else {
+        record += Number(screen.getAttribute("value")) + " / ";
+    }
+
+    historial.setAttribute("value", record);
+
+    if (Number(screen.getAttribute("value")) !== 0) {
         if (memory == 0) {
             memory = Number(screen.getAttribute("value"));
         } else {
-            memory /= Number(screen.getAttribute("value"));
+            if (lastOperator !== "=") {
+                memory /= Number(screen.getAttribute("value"));
+            }
         }
     }
     screen.setAttribute("value", 0);
     lastOperator = "/";
-    record += memory + " / ";
-    history.setAttribute("value", record);
 }
 
+/**
+ * Gives the result of an  previous operation
+ */
 document.getElementById("equal").onclick = function () {
+
     switch (lastOperator) {
         case "+":
             memory += Number(screen.getAttribute("value"));
-            lastOperator = "";
+            lastOperator = "=";
             break;
 
         case "-":
             memory -= Number(screen.getAttribute("value"));
-            lastOperator = "";
+            lastOperator = "=";
             break;
 
         case "*":
             memory *= Number(screen.getAttribute("value"));
-            lastOperator = "";
+            lastOperator = "=";
             break;
 
         case "/":
             memory /= Number(screen.getAttribute("value"));
-            lastOperator = "";
+            lastOperator = "=";
             break;
     }
-
+    record += Number(screen.getAttribute("value")) + " = " + memory + "\n";
+    historial.setAttribute("value", record);
     screen.setAttribute("value", memory);
-    record += memory + " = " + "\n";
-    history.setAttribute("value", record);
+
 };
 
+/**
+ * Cleans the calculator memory
+ */
 document.getElementById("clear").onclick = function () {
     memory = 0;
     screen.setAttribute("value", 0);
+    record = "";
+    historial.setAttribute("value", 0);
 };
